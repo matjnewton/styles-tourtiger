c64cdc0cecd5c7917d3fdb94d79e0bf0f40d9bd8
Andriy <a.rysan@yahoo.com>
2017-02-03 20:12:44 +0200

    connect plugin to Github repo

--- plugin.php
+++ plugin.php
@@ -4,8 +4,9 @@ Plugin Name: Styles: tourtiger
 Plugin URI: http://stylesplugin.com
 Description: Add Customize options to the <a href="http://wordpress.org/extend/themes/tourtiger" target="_blank">tourtiger theme</a> using the <a href="http://wordpress.org/extend/plugins/styles/" target="_blank">Styles plugin</a>.
 Version: 1.0
-Author: Paul Clark, 10up
+Author: Paul Clark, 10up, tourtiger
 Author URI: http://pdclark.com
+GitHub Plugin URI: https://github.com/matjnewton/styles-tourtiger
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
